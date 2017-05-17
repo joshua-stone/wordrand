7bce668b83022fac202e55cb237885a26876d283
Joshua Stone <joshua.gage.stone@gmail.com>
2017-05-17 03:11:55 -0400

    Read dict file into vector of strings for easy access

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use std::path::Path;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
 
 fn main() {
     let dict = Path::new("/usr/share/dict/words");
+    let file = match File::open(&dict) {
+        Ok(f) => f,
+        Err(message) => panic!("Couldn't open dictionary file: {}: {}", dict.display(), message)
+    };
+
+    let words: Vec<String> = BufReader::new(file).lines().map(|x| x.unwrap()).collect();
 }
