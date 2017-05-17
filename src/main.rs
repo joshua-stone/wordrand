57e4593bdbc6dbe033658f417f085d1d1bd30991
Joshua Stone <joshua.gage.stone@gmail.com>
2017-05-17 03:41:04 -0400

    Join words so they can be read on one line

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::path::Path;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
-extern crate rand;
 
+extern crate rand;
 use rand::{thread_rng, sample};
+
 fn main() {
     let word_count = 3;
 
     let dict = Path::new("/usr/share/dict/words");
+
     let file = match File::open(&dict) {
         Ok(f) => f,
         Err(message) => panic!("Couldn't open dictionary file: {}: {}", dict.display(), message)
@@ -17,4 +19,8 @@ fn main() {
 
     let mut rng = thread_rng();
     let random_words = sample(&mut rng, words, word_count);
+
+    
+    println!("{}", random_words.join("."));
+
 }
