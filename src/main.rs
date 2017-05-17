1f4fd85b367b941f2585ad843f187b81e2380a97
Joshua Stone <joshua.gage.stone@gmail.com>
2017-05-17 03:31:28 -0400

    Use rand crate to generate random words

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::path::Path;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
+extern crate rand;
 
+use rand::{thread_rng, sample};
 fn main() {
+    let word_count = 3;
+
     let dict = Path::new("/usr/share/dict/words");
     let file = match File::open(&dict) {
         Ok(f) => f,
@@ -10,4 +14,7 @@ fn main() {
     };
 
     let words: Vec<String> = BufReader::new(file).lines().map(|x| x.unwrap()).collect();
+
+    let mut rng = thread_rng();
+    let random_words = sample(&mut rng, words, word_count);
 }
