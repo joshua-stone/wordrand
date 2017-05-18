36d35e69cd48b29b8761c0483e6429684c92d8ea
Joshua Stone <joshua.gage.stone@gmail.com>
2017-05-18 11:34:25 -0400

    Clean up code, use more type annotations, and change default --number value

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 
 extern crate clap;
-use clap::{App};
+use clap::App;
 
 extern crate rand;
 use rand::{Rng, thread_rng};
@@ -20,27 +20,25 @@ fn main() {
                         )
                         .get_matches();
 
-    let lines = args.value_of("lines").unwrap_or("1").parse().unwrap();
+    let lines: usize = args.value_of("lines").unwrap_or("1").parse().unwrap();
     let word_count: usize = args.value_of("number").unwrap_or("3").parse().unwrap();
     let separator: &str = args.value_of("separator").unwrap_or(".");
 
     let dict = Path::new("/usr/share/dict/words");
 
-    let file = match File::open(&dict) {
-        Ok(f) => f,
+    let words: Vec<String> = match File::open(&dict) {
+        Ok(f) => BufReader::new(f).lines().map(|x| x.unwrap()).collect(),
         Err(message) => panic!("Couldn't open dictionary file: {}: {}", dict.display(), message)
     };
 
-    let words: Vec<String> = BufReader::new(file).lines().map(|x| x.unwrap()).collect();
-
     let mut rng = thread_rng();
-
     let mut indices = HashSet::with_capacity(word_count);
+
     for _ in 0..lines {
         while indices.len() < word_count {
             indices.insert(rng.gen_range(0, words.len()));
         }
-        let random_words: Vec<_> = indices.iter().map(|i| words[*i].as_str()).collect();
+        let random_words: Vec<&str> = indices.iter().map(|i| words[*i].as_str()).collect();
         println!("{}", random_words.join(separator));
         indices.clear();
     }
